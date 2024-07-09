49d32d1fe2d3f066bd7c30155ba0c68303a4f920
brunobahri <brunobahri27@gmail.com>
2024-07-09 18:56:32 -0300

    chore: src and project structure create

---
+++ src/test/java/com/brunobahri/tasklist/TaskListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobahri.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
