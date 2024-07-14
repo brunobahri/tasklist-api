b3a5a1735a92aef63b3a9a6a3a5a5054bebccfa3
brunobahri <brunobahri27@gmail.com>
2024-07-13 22:29:42 -0300

    fix: loop bug on authentication

--- src/main/java/com/brunobahri/tasklist/service/UserService.java
+++ src/main/java/com/brunobahri/tasklist/service/UserService.java
@@ -5,7 +5,6 @@ import com.brunobahri.tasklist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
